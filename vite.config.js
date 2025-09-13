3393540301f4bcc2a66e403ab3116fa6d257cb40
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-09-13 18:08:03 +0530

    btn

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+export default {
+    build: {
+      rollupOptions: {
+        input: {
+          main: './index.html',
+          second: './second.html',
+          third: './third.html'
+        }
+      }
+    }
+  };
+  
